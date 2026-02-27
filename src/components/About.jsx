d321b72e40cdfe8091fae292d56bc62d13f9ad79
THE-Amrit-mahto-05 <yadhukumar045@gmail.com>
2026-02-27 06:18:47 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,9 @@ const About = () => {
           className="text-center"
         >
           <h2 className="text-3xl md:text-4xl font-medium text-[#1E1E1E] leading-[1.8] tracking-tight font-cursive max-w-4xl mx-auto italic">
-            I have contributed to open-source projects including mongosh and React, and participated in Hacktoberfest 2025, where I submitted 10 pull requests, with 9 successfully merged. I’ve worked on several AI/ML and computer vision projects, including large-scale image annotation of 8,700+ images using Roboflow, and enjoy experimenting with modern AI tools to accelerate development. During my summer vacation, I built a notes sharing platform end-to-end and gained exposure to cloud technologies (AWS) and CI/CD workflows using GitHub Actions. I’m also a Google Developer Group (GDG) member since 2024, actively engaging with the developer community through continuous learning and collaboration.
+            I have contributed to open-source projects including mongosh and React, and participated in Hacktoberfest 2025, where I submitted 10 pull requests, with 9 successfully merged. I’ve worked on several AI/ML and computer vision projects, 
+            including large-scale image annotation of 8,700+ images using Roboflow, and enjoy experimenting with modern AI tools to accelerate development. During my summer vacation, I built a notes sharing platform end-to-end and I also gained exposure to cloud technologies (AWS) and CI/CD workflows using GitHub Actions. 
+            I’m also a Google Developer Group (GDG) member since 2024, actively engaging with the developer community through continuous learning and collaboration.
           </h2>
         </motion.div>
       </div>
